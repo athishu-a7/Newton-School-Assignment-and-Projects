ede37c6c0717ee9ae56caddcfb66d0f58a30d564
Athishu Kezho <68458792+athishu-a7@users.noreply.github.com>
2022-12-14 08:46:30 +0530

    commit

---
+++ Projects/Note Keeping/script.js
@@ -0,0 +1,9 @@
+const addNotesButton = document.getElementById("add-notes");
+const inputTitle = document.getElementById('title');
+
+addNotesButton.addEventListener('click', () => {
+    console.log(inputTitle.value)
+    const newNotesTiltle = document.createElement('p');
+    newNotesTiltle.innerText
+    console.log("adding notes")
+})
