92c311ca49b08ce42f1a91261e3218cc0b7f8bb4
Athishu Kezho <68458792+athishu-a7@users.noreply.github.com>
2022-12-20 21:28:11 +0530

    commit changes

--- Projects/Note Keeping/script.js
+++ Projects/Note Keeping/script.js
@@ -1,9 +1,19 @@
 const addNotesButton = document.getElementById("add-notes");
 const inputTitle = document.getElementById('title');
+const displayNotesSection = document.getElementById('notes')
 
+// addNoteButton onClick
 addNotesButton.addEventListener('click', () => {
-    console.log(inputTitle.value)
+// step 1 - get the value entered by the user
+
+// step 2 - create an HTML tag for displaying the notes
     const newNotesTiltle = document.createElement('p');
-    newNotesTiltle.innerText
-    console.log("adding notes")
+
+    const deleteNoteButton = document.createElement('button')
+
+// step 3 - add the value that the user entered to the new tag
+    newNotesTiltle.innerText = inputTitle.value;
+  
+// step 4 - append the created notes tag into display section    
+    displayNotesSection.appendChild(newNotesTiltle);
 })
